92db3469297905d8a1e5bdb3b0e42a87293278d4
Simon Riou <80064281+simonriou@users.noreply.github.com>
2025-12-17 16:01:31 +0100

    Confirmed loss weights

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -28,7 +28,7 @@ MODEL_DIR = ROOT / "data" / "models"
 
 LOG_DIR = ROOT / "experiments" / "logs"
 CHECKPOINT_DIR = ROOT / "experiments" / "checkpoints"
-MODEL_NAME = "batch32-custom"
+MODEL_NAME = "batch32-weighted"
 SAVE_DENOISED = True
 SAVE_NOISY = True
 
