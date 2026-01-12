c467d304d788b254bb865deb338e100c797e5a8e
Simon Riou <80064281+simonriou@users.noreply.github.com>
2026-01-12 15:06:33 +0100

    Trying dilation for bottleneck

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -28,7 +28,7 @@ MODEL_DIR = ROOT / "data" / "models"
 
 LOG_DIR = ROOT / "experiments" / "logs"
 CHECKPOINT_DIR = ROOT / "experiments" / "checkpoints"
-MODEL_NAME = "waveform-3"
+MODEL_NAME = "waveform-4"
 SAVE_DENOISED = True
 SAVE_NOISY = True
 
